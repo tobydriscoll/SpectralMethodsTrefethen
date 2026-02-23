40d734cc8f9c93592056609e5b4e8d4a8b831b64
Toby Driscoll <driscoll@udel.edu>
2026-02-23 18:06:40 -0500

    use DCT and DST for chebfft

--- src/chebfft.jl
+++ src/chebfft.jl
@@ -1,29 +1,23 @@
-function fftdiff(V::AbstractVector{<:Real}, N::Integer)
-    V̂ = rfft(V)
-    ξ = [0:N-1; 0]
-    return real(V̂), irfft(im * ξ .* V̂, 2N)
-end
-
-function fftdiff(V::AbstractVector{<:Complex}, N::Integer)
-    V̂ = fft(V)
-    ξ = [0:N-1; 0; -N+1:-1]
-    return V̂, ifft(im * ξ .* V̂)
-end
+DCT(v) = FFTW.r2r(v, FFTW.REDFT00)
+DST(v) = FFTW.r2r(v, FFTW.RODFT00)
 
 """
     chebfft(v)
 Differentiate values given at Chebyshev points via the FFT.
 """
 function chebfft(v)
-    # This can be rewritten to use dct in order to exploit symmetry more efficiently.
     N = length(v) - 1
-    N == 0 && return [0]
+    (N == 0) && return zero(v)
     x = [cospi(k / N) for k in 0:N]
-    V = [v; reverse(v[2:N])]              # transform x -> theta
-    V̂, W = fftdiff(V, N)
+    v̂ = DCT(v)
     w = similar(v)
-    @. w[2:N] = -W[2:N] / sqrt(1 - x[2:N]^2)      # transform theta -> x
-    w[1] = N/2 * V̂[N+1] + sum(i^2 * V̂[i+1] for i in 0:N-1) / N
-    w[N+1] = (-1)^(N+1) * N/2 * V̂[N+1] + sum((-1)^(i+1) * i^2 * V̂[i+1] for i in 0:N-1) / N
+    if N > 1
+        ξ = 1:N-1
+        s = @. -sqrt(1 - x[2:N]^2)    # chain rule denominator
+        w[2:N] = DST(-ξ .* v̂[2:N]) ./ (2N * s)
+    end
+    w[1] = N/2 * v̂[N+1] + sum(i^2 * v̂[i+1] for i in 0:N-1) / N
+    w[N+1] = (-1)^(N+1) * N/2 * v̂[N+1] +
+        sum((-1)^(i+1) * i^2 * v̂[i+1] for i in 0:N-1) / N
     return w
 end

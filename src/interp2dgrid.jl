1434b53b3166bed8824f00cbacb71b160c75fc2e
Toby Driscoll <driscoll@udel.edu>
2026-02-24 13:46:02 -0500

    change notation in interp2dgrid

--- src/interp2dgrid.jl
+++ src/interp2dgrid.jl
@@ -1,4 +1,4 @@
-function interp2dgrid(V, method1, method2, x, y)
-    W = mapslices(v -> method1(v).(x), V; dims=[1])
-    return mapslices(w -> method2(w).(y), W; dims=[2])
+function interp2dgrid(V, method1, method2, ξ, η)
+    W = mapslices(v -> method1(v).(ξ), V; dims=[1])
+    return mapslices(w -> method2(w).(η), W; dims=[2])
 end

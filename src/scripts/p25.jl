413b95a25dadc91fdd464aa6a70941ca35ceccf0
Toby Driscoll <driscoll@udel.edu>
2026-02-26 11:15:51 -0500

    revamp stability region code

--- src/scripts/p25.jl
+++ src/scripts/p25.jl
@@ -10,48 +10,46 @@ function p25()
 
     z = [cispi(x) for x in (0:200) / 100]
     # Adams–Bashforth, orders 1-3:
-    r = z .- 1
-    for f in [z -> 1, z -> (3 - 1 / z) / 2, z -> (23 - 16 / z + 5 / z^2) / 12]
-        lines!(ax[1, 1], reim(@. r / f(z))...)
+    ρz = @. z - 1
+    for σ in Polynomial.([[1], [-1, 3] / 2, [5, -16, 23] / 12])
+        w = @. ρz / σ(z)
+        lines!(ax[1, 1], reim(w)...)
+        @. ρz *= z
     end
     ax[1, 1].title = "Adams–Bashforth"
 
     # Adams–Moulton, orders 3-6:
-    sfun = [z -> (5 * z + 8 - 1 / z) / 12,
-        z -> (9 * z + 19 - 5 / z + 1 / z^2) / 24,
-        z -> (251 * z + 646 - 264 / z + 106 / z^2 - 19 / z^3) / 720]
-    for f in sfun
-        lines!(ax[1, 2], reim(@. r / f(z))...)
+    ρz = @. z^2 - z
+    for σ in Polynomial.([
+            [-1, 8, 5] / 12,
+            [1, -5, 19, 9] / 24,
+            [-19, 106, -264, 646, 251] / 720,
+            [27, -173, 482, -798, 1427, 475] / 1440
+        ])
+        w = @. ρz / σ(z)
+        lines!(ax[1, 2], reim(w)...)
+        @. ρz *= z
     end
-    d = @. 1 - 1 / z
-    p = Polynomial([1, -1 / 2, -1 / 12, -1 / 24, -19 / 720, -3 / 160])
-    lines!(ax[1, 2], reim(@. d / p(d))...)
     ax[1, 2].title = "Adams–Moulton"
 
     # Backward differentiation, orders 1-6:
-    r = zeros(size(d))
-    for i in 1:6
-        r += (d .^ i) / i
+    d = @. 1 - 1 / z
+    r = zero(d)
+    for n in 1:6
+        @. r += (d ^ n) / n
         lines!(ax[2, 1], reim(r)...)
     end
     ax[2, 1].title = "backward differentiation"
 
     # Runge-Kutta, orders 1-4:
-    taylor = [Polynomial(1 ./ factorial.(0:n)) for n in 1:4]
-    w = complex(zeros(4))
-    W = repeat(w, 1, length(z))
-    for i in 2:length(z)
-        w[1] -= (taylor[1](w[1]) - z[i])
-        w[2] -= (taylor[2](w[2]) - z[i]^2) / taylor[1](w[2])
-        w[3] -= (taylor[3](w[3]) - z[i]^3) / taylor[2](w[3])
-        w[4] -= (taylor[4](w[4]) - z[i]^4) / taylor[3](w[4])
-        W[:, i] .= w
-    end
-    for w in eachrow(W)
+    T = [Polynomial(1 ./ factorial.(0:n)) for n in 0:4]
+    w = zero(z)
+    for n in 1:4
+        for i in 1:length(z)-1
+            w[i+1] = w[i] - (T[n+1](w[i]) - z[i+1]^n) / T[n](w[i])
+        end
         lines!(ax[2, 2], reim(w)...)
     end
     ax[2, 2].title = "Runge–Kutta"
     return fig
 end
-
-p25()

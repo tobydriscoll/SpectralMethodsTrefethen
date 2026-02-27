b31522ed1eb53680f2a63f697df4943cd5b938fc
Toby Driscoll <driscoll@udel.edu>
2026-02-27 15:22:37 -0500

    extend clencurt to other float types

--- src/clencurt.jl
+++ src/clencurt.jl
@@ -2,19 +2,19 @@
     clencurt(N)
 Nodes and weights for Clenshaw–Curtis quadrature.
 """
-function clencurt(N)
-    q = [i / N for i in 0:N]
+function clencurt(N, T::Type{<:AbstractFloat}=Float64)
+    q = [T(i) / N for i in 0:N]
     x = cospi.(q)
-    w = zeros(N+1)
-    v = ones(N-1)
+    w = zeros(T, N+1)
+    v = ones(T, N-1)
     if iseven(N)
-        w[1] = w[N+1] = 1 / (N^2 - 1)
+        w[1] = w[N+1] = one(T) / (N^2 - 1)
         for k in 1:div(N, 2)-1
             @. v -= 2cospi(2k * q[2:N]) / (4k^2 - 1)
         end
         @. v -= cospi(N * q[2:N]) / (N^2 - 1)
     else
-        w[1] = w[N+1] = 1 / N^2
+        w[1] = w[N+1] = one(T) / N^2
         for k in 1:div(N-1, 2)
             @. v -= 2cospi(2k * q[2:N]) / (4k^2 - 1)
         end

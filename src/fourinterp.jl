63bc62e8a06f37eac3ac1daa484a89b51665dcd6
Toby Driscoll <driscoll@udel.edu>
2026-02-27 14:30:19 -0500

    fix instability in Fourier interpolation

--- src/fourinterp.jl
+++ src/fourinterp.jl
@@ -1,14 +1,13 @@
 """
     fourinterp(v)
-Create a callable interpolant for the vector `v` of values given at equispaced (Fourier) nodes in (0, 2π].
+Create a callable interpolant for the vector `v` of values given at equispaced (Fourier)
+nodes in (0, 2π].
 """
 function fourinterp(v)
     N = length(v)
     @assert(iseven(N), "N must be even")
-    SN(x) = sin(N * x / 2) / (N * tan(x / 2))    # δ interpolant
-    return function(x)
-        t = [SN(x - m * 2π / N) for m in 1:N]
-        hit = isnan.(t)
-        any(hit) ? v[findfirst(hit)] : dot(v, t)
-    end
+    h = 2π / N
+    # Periodic sinc function:
+    SN(x) = iszero(mod(x, 2π)) ? 1 : sin(N * x / 2) / (N * tan(x / 2))
+    return x -> sum(v[j] * SN(x - j * h) for j in 1:N)
 end

3a2f9db343d15fa49f8cabfaa76315c49ade0ff9
Toby Driscoll <driscoll@udel.edu>
2026-03-06 10:10:59 -0500

    ensure floating point in all cases of SN

--- src/fourinterp.jl
+++ src/fourinterp.jl
@@ -8,6 +8,6 @@ function fourinterp(v)
     @assert(iseven(N), "N must be even")
     h = 2π / N
     # Periodic sinc function:
-    SN(x) = iszero(mod(x, 2π)) ? 1 : sin(N * x / 2) / (N * tan(x / 2))
+    SN(x) = iszero(mod(x, 2π)) ? 1.0 : sin(N * x / 2) / (N * tan(x / 2))
     return x -> sum(v[j] * SN(x - j * h) for j in 1:N)
 end

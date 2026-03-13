8a366520330a07ec92c63b94dba3bd5a2039c54c
Toby Driscoll <driscoll@udel.edu>
2026-03-13 09:00:27 -0400

    loop for constructing cheb diff matrix

--- src/cheb.jl
+++ src/cheb.jl
@@ -5,13 +5,18 @@ Chebyshev differentiation matrix and grid.
 function cheb(N)
     (N == 0) && return zeros(1, 1), [1.0]
     x = [sinpi(k / 2N) for k in N:-2:-N]
-    c = [2; ones(N-1); 2]
-    @. c[2:2:end] = -c[2:2:end]
+    c(i) = (i == 0 || i == N) ? 2 : 1
+    neg1(i) = iseven(i) ? 1 : -1
     # compute the off-diagonal entries
-    D = [c[i] / (c[j] * (x[i] - x[j] + (i==j))) for i in eachindex(x), j in eachindex(x)]
+    D = zeros(N+1, N+1)
+    for i in 0:N, j in 0:N
+        if i != j
+            D[i+1, j+1] = neg1(i+j) * c(i) / (c(j) * (x[i+1] - x[j+1]))
+        end
+    end
     # negative sum trick to get the diagonal entries
-    for i in eachindex(x)
-        D[i, i] = -sum(D[i, j] for j in eachindex(x) if j != i)
+    for i in 0:N
+        D[i+1, i+1] = -sum(D[i+1, :])
     end
     return D, x
 end
